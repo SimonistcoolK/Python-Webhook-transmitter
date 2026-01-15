1843c69d83357d87a4b05a936eb5430876c0fd8c
SimonistcoolK <simonkranabetter+github@gmail.com>
2026-01-15 20:12:05 +0100

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,9 @@
+import requests, time 
+msg = input('Enter your message:')
+webhook_url = input('Enter your webhook URL:')
+data = {
+    "content": msg
+}
+response = requests.post(webhook_url, json=data)
+print("sent:", msg, "to webhook no.", webhook_url)
+time.sleep(3)
