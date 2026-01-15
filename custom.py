eb0339045cb617da1333e9f9cbaa5980bb1432f6
SimonistcoolK <simonkranabetter+github@gmail.com>
2026-01-15 20:32:42 +0100

    Refactor custom.py to improve message transmission

--- custom.py
+++ custom.py
@@ -1,4 +1,5 @@
 import requests, time 
+print("Transmitter")
 msg = input('Enter your message:')
 webhook_url = input('Enter your webhook URL:')
 data = {
@@ -6,4 +7,5 @@ data = {
 }
 response = requests.post(webhook_url, json=data)
 print("sent:", msg, "to webhook no.", webhook_url)
+
 time.sleep(3)
